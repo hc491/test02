77312b45a604271a3f62c2357ea14b54e93d01ae
Chau <hoaichau20@hotmail.com>
2025-10-08 10:34:45 +0200

    first save

---
+++ exos.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+
+
+// Exemple : afficher 5 fois "Bonjour"
+//for (let i = 0; i < 5; i++) {
+//  console.log("Bonjour 👋");
+//}
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
